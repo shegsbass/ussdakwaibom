6b1d6e3daed434ba9f74202727b08e953e7a48fc
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2018-11-15 12:40:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -157,10 +157,9 @@ else if ($text =="1") {
    elseif($text == "1*1*1"){
        // displays the polling unit for Abak Urban I
       
-       $response = "END \n
-                          *   *** *** \n
-                         ***  *** *   \n
-                        *   * *   ***  \n";
+       $response = "  *   *** *** \n
+                     ***  *** *   \n
+                    *   * *   ***  \n";
                         
        $response .= $message;
        
