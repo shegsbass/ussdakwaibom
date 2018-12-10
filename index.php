143b1e6d2d83de0c51d89718aeda6d7cf1ebc794
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2018-12-10 17:00:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ $phoneNumber = $_POST["phoneNumber"];
 $text = $_POST["text"];
 $message = "Because Akwa Ibom Deserves Better.\n";
 
-$display = "<pre style = \"line-height:6px; line-weight:6px\"> ##     ##     ###\n
+$display = " ##     ##     ###\n
 #  #   #  #   #\n
 ####   ###    #\n
-#  #   #       ###</pre>\n";
+#  #   #       ###\n";
 /**<pre style = \"line-height:6px; line-weight:6px\">
 
 $display = "
